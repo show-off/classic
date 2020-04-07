7abfb80ae03e08abbb37216f6cfc7698edd744c7
Siddharth Venu <siddharthvvenu@gmail.com>
2020-04-08 00:18:15 +0530

    Add intro component, apply styles to buttons in the webpage

---
+++ src/components/intro.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./intro.css"
+
+export const Intro = () => (
+  <section id="intro">
+    <p id="intro-text" className="text-center">
+      App Developer. Open Source Contributor.
+      <br />
+      Architect of beautiful UIs.
+    </p>
+    <button className="outline">Who are you?</button>
+  </section>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react"
 import "../styles/global.css"
 import { Navbar } from "../components/navbar"
+import { Intro } from "../components/intro"
 
 const IndexPage = () => (
   <Fragment>
     <Navbar />
+    <Intro />
   </Fragment>
 )
 
