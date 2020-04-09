6b50d5aadde18076ba8829f80823cb94100118d3
Siddharth Venu <siddharthvvenu@gmail.com>
2020-04-09 14:40:21 +0530

    Include username and email in Travis builds, separate deploy commands into a shell script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git config user.name "Siddharth Venu"
+git config user.email "siddharthvvenu@gmail.com"
+gatsby build --prefix-paths
+gh-pages -d public -r https://$GH_TOKEN@github.com/show-off/classic.git
